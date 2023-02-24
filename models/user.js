ad6e237825c0619376791867512516d479d4e601
Jason <jasoncmcvicar@gmail.com>
2023-02-24 17:54:16 -0600

    fixed register method to contain last login and finished get method for one username

--- models/user.js
+++ models/user.js
@@ -23,9 +23,10 @@ class User {
                           first_name,
                           last_name,
                           phone,
-                          join_at)
+                          join_at,
+                          last_login_at)
         VALUES
-          ($1, $2, $3, $4, $5, current_timestamp)
+          ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
         RETURNING username, password, first_name, last_name, phone, join_at`,
       [username, hashedPassword, first_name, last_name, phone]);
 
@@ -97,6 +98,26 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+
+    const result = await db.query(
+      `SELECT username,
+              first_name,
+              last_name,
+              phone,
+              join_at,
+              last_login_at
+        FROM users
+        WHERE username = $1`,
+        [username]
+    );
+
+    const user = result.rows[0];
+
+    if (!user) {
+      throw new NotFoundError(`No such user: ${username}`);
+    }
+
+    return user;
   }
 
   /** Return messages from this user.
