1cdd3cfad3d3db3049e888ad237aae12ef86eb82
anela <anela89@gmail.com>
2023-03-09 14:19:34 -0800

    Created POST route for sending a message

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,12 @@
 const Router = require("express").Router;
 const router = new Router();
 
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth.js");
+const User  = require("../models/user");
+const Message = require("../models/message");
+const { NotFoundError } = require("../expressError");
+
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -23,7 +29,25 @@ const router = new Router();
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+  const to_username = req.body.to_username;
+  const body = req.body.body;
 
+  try {
+    const result = await User.get(to_username);
+  } catch(err) {
+    throw new NotFoundError(err);
+  }
+
+  try {
+    const from_username = res.locals.user.username;
+    let messageData = { from_username, to_username, body};
+    const result = await Message.create(messageData);
+    return res.json({message: result});
+  } catch(err) {
+    return next(err);
+  }
+});
 
 /** POST/:id/read - mark message as read:
  *
