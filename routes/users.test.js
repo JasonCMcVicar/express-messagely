0f1afafe78072a7816f15ea3b265b0593866f509
Jason <jasoncmcvicar@gmail.com>
2023-03-15 18:29:00 -0500

    working through tests for users routes, got test to pass for user loged in with a token, to be continued

---
+++ routes/users.test.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const request = require("supertest");
+const jwt = require("jsonwebtoken");
+
+const app = require("../app");
+const db = require("../db");
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config.js");
+
+describe("Users Routes Test", function () {
+
+  let testUserToken;
+
+  beforeEach(async function () {
+    await db.query("DELETE FROM messages");
+    await db.query("DELETE FROM users");
+
+    let u1 = await User.register({
+      username: "test1",
+      password: "password",
+      first_name: "Test1",
+      last_name: "Testy1",
+      phone: "+14155550000",
+    });
+    let u2 = await User.register({
+      username: "test2",
+      password: "password",
+      first_name: "Test2",
+      last_name: "Testy2",
+      phone: "+14155550001",
+    });
+
+    const testUser1 = { username: "test1" };
+    testUserToken = jwt.sign(testUser1, SECRET_KEY);
+
+  });
+
+  describe("GET /users/", function () {
+    test("can get list of all users", async function () {
+      let response = await request(app)
+        .get("/users/")
+        .query({ _token: testUserToken });
+
+      expect(response.body).toEqual({
+        users:
+          [{
+            username: "test1",
+            first_name: "Test1",
+            last_name: "Testy1",
+          },
+          {
+            username: "test2",
+            first_name: "Test2",
+            last_name: "Testy2",
+          }]
+      });
+    });
+  });
+
+
+//{users: [{username, first_name, last_name}, ...]}
+
+
+});
+
+afterAll(async function () {
+  await db.end();
+});
