6bf08dc0fb4607e4eb0bb23d3a51218eff8402bd
anela <anela89@gmail.com>
2023-03-16 15:19:15 -0700

    Added last_login_at as a returned value for register method

--- models/user.js
+++ models/user.js
@@ -27,7 +27,7 @@ class User {
                           last_login_at)
         VALUES
           ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
-        RETURNING username, password, first_name, last_name, phone, join_at`,
+        RETURNING username, password, first_name, last_name, phone, join_at, last_login_at`,
       [username, hashedPassword, first_name, last_name, phone]);
 
     return result.rows[0];
